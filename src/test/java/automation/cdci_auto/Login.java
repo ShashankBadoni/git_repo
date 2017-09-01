91a895087930726e767504538f25aac88b580707
shashank <shashank.badoni@gmail.com>
2017-09-01 20:24:27 +0530

    My local recommit

---
+++ src/test/java/automation/cdci_auto/Login.java
@@ -0,0 +1,12 @@
+package automation.cdci_auto;
+
+import org.testng.annotations.Test;
+
+public class Login {
+
+	@Test
+	public void typeText(){
+		System.out.println("=== My first programe ===");
+	}
+	
+}
